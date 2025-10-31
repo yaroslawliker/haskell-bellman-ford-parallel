1cc06b9eef38737f12af12c5973436a872806049
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-10-31 19:25:32 +0200

    (fix): fixed relaxAllNodesParallel: missing last arcs while deviding

--- src/Parallel/ParallelBellmanFord.hs
+++ src/Parallel/ParallelBellmanFord.hs
@@ -1,12 +1,12 @@
-module Parallel.ParallelBellmanFord (relaxAllNodesParallel) where
+module Parallel.ParallelBellmanFord (relaxAllNodesParallel, bellmanFordParralel) where
 
 import Control.Parallel.Strategies;
 
-import Serial.Graph (Graph(..))
+import Serial.Graph (Graph(..), Node, getNodes)
 
 import Serial.BellmanFord (
     CostMap(..),
-    relaxCost, relaxAllNodes, removeDuplicates);
+    relaxAllNodes, removeDuplicates, initCosts);
 
 -- Takes sub-costmap, a full graph and returns an update costmap
 relaxAllNodesWrapper :: (CostMap, Graph) -> CostMap
@@ -20,37 +20,16 @@ relaxAllNodesParallel :: Int -> CostMap -> Graph -> CostMap
 relaxAllNodesParallel procN costMap (Graph arcs) =
     removeDuplicates $ concat $ parMap rdeepseq relaxAllNodesWrapper subTasks
     where
-        arcsPerProc = length costMap `div` procN
-        -- Example
-        -- 1 2 3 4 5 6 7 8 9 arcs
-        -- nodesN = 4;
-        --
-        -- arcsPerProc = 9 / 4 = 2
-        --
-        -- p = 0, start = 0*2 = 0
-        -- drop 0 -> 1 2 3 4 5 6 7 8 9
-        -- take 2 -> 1 2
-        --
-        -- p = 1, start = 1*2 = 2
-        -- drop 2 -> 3 4 5 6 7 8 9
-        -- take 2 -> 3 4
-        --
-        -- p = 2, start = 2*2 = 4
-        -- drop 4 -> 5 6 7 8 9
-        -- take 2 -> 5 6
-        -- 
-        -- p = 3, start = 3*2 = 6
-        -- drop 6 -> 7 8 9
-        -- -> 7 8 9
-
+        arcsPerProc = length arcs `div` procN
         subGraphs = [
                 Graph subTask |
-                p <- [0..(procN-1)],
+                let lastP = procN-1,
+                p <- [0..lastP],
                 let start = p*arcsPerProc,
                 let dropped = drop start arcs,
                 let subTask =
                         -- Check is needed to give the last process remaining arcs
-                        if length dropped < arcsPerProc*2
+                        if p == lastP
                             then dropped
                             else take arcsPerProc dropped
             ]
