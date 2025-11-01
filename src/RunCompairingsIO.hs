2d7b3f9086e9263c118455750215c8dc2e438de9
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-01 22:43:02 +0200

    (feature): added better output

--- src/RunCompairingsIO.hs
+++ src/RunCompairingsIO.hs
@@ -8,6 +8,7 @@ import ParallelBellmanFord (bellmanFordParralel)
 
 import RandomGraph (generateRandomGraph)
 import Control.DeepSeq (deepseq)
+import Text.Printf (printf)
 
 runCompairing :: Int -> StdGen -> IO (Double, Double)
 runCompairing nodeN g = do
@@ -15,33 +16,33 @@ runCompairing nodeN g = do
     -- Graph generation
     let arcN = nodeN*nodeN `div` 4
 
-    print "------------"
-    print "Running for:"
-    print $ "Nodes: " ++ show nodeN
-    print $ "Arcs: " ++ show arcN
+    printf $ replicate 50 '-' ++ "\n"
+    printf $ "Running for:    " ++ show nodeN ++ " nodes    and    " ++ show arcN  ++ " arcs: " ++ "\n"
 
     
+    -- Graph generation
     genStartTime <- getCurrentTime
     let graph = generateRandomGraph nodeN arcN (-1000) 1000 True g -- 1000*1000 = 1 000 000
     genEndTime <- deepseq graph getCurrentTime
-
     let genDuration = diffUTCTime genEndTime genStartTime
-    print ("Graph generation time: " ++ show genDuration)
+
 
     -- Serial Bellman-Ford algorythm
     serialStartTime <- getCurrentTime
     let serialSol = bellmanFord graph 1
     serialEndTime <- deepseq serialSol getCurrentTime
-    
-    let serialDuration = diffUTCTime serialEndTime serialStartTime 
-    print ("Serial Bellman-Ford time: " ++ show serialDuration)
-    
+    let serialDuration = diffUTCTime serialEndTime serialStartTime   
+
+
     -- Parallel Bellman-Ford algorythm
     parallelStartTime <- getCurrentTime
     let parallelSol = bellmanFordParralel 4 graph 1
     parallelEndTime <- deepseq parallelSol getCurrentTime
     
     let parallelDuration = diffUTCTime parallelEndTime parallelStartTime 
-    print ("Parallel Bellman-Ford time: " ++ show parallelDuration)
+
+    -- Outputs
+    printf $ "Graph generation: " ++ show genDuration ++ "\n"
+    printf $ "Serial " ++ show serialDuration ++ "    Parallel " ++ show parallelDuration ++ "\n"
 
     return (realToFrac serialDuration :: Double, realToFrac parallelDuration :: Double)
