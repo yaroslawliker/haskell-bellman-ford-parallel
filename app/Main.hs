8b25e76b93a6e980066cb2694d94a8455e544ba4
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-11-01 22:43:12 +0200

    (feature): added more examples

--- app/Main.hs
+++ app/Main.hs
@@ -10,8 +10,9 @@ main = do
 
     let randomGenerator = mkStdGen 17
 
-    r1 <- runCompairing 200 randomGenerator
-    -- r2 <- runCompairing 100 randomGenerator
-    -- r3 <- runCompairing 200 randomGenerator
+    r1 <- runCompairing 10 randomGenerator
+    r2 <- runCompairing 100 randomGenerator
+    r3 <- runCompairing 200 randomGenerator
+    r4 <- runCompairing 400 randomGenerator
 
     return ()
